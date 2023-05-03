7c50ccc03b8deb68a86f2e79d0642ad6eb0cc669
Daniel de Mesquita <108764201+demesquitadaniel@users.noreply.github.com>
2023-05-03 09:49:22 -0300

    update May 2nd 2023

--- Fundamentals/02_comments.js
+++ Fundamentals/02_comments.js
@@ -3,13 +3,14 @@ console.log('The comment above refers to this line here.')
 
 console.log('Or may be on the side, like here: ') // Here is the side-comment.
 
-/* You can
-also have
-multiple-line
-comments.
+/*  You can
+    also have
+    multiple-line
+    comments.
 */
 console.log('Above we have the comment for this code here: ')
 
-// Avoid redundant, unreadable, obvious or confuse comments.
-// Name files and variables, everything, in a clear and informative way. 
-// Make it easier for any programmer to read and understand your comments.
+/*  Avoid redundant, unreadable, obvious or confuse comments.
+    Name files and variables, everything, in a clear and informative way. 
+    Make it easier for any programmer to read and understand your comments.
+*/
